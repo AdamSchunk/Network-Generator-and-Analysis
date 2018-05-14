93de50c8318eb5421ddb617c54fb79d39b4764e1
adam <adamschunk@gmail.com>
2018-05-14 15:25:10 -0400

    finished code to run net

--- NetworkGenerator/src/network/NetworkRunner.java
+++ NetworkGenerator/src/network/NetworkRunner.java
@@ -7,24 +7,10 @@ import java.util.Random;
 import javax.naming.InitialContext;
 
 public class NetworkRunner {
-
-	/*
-	 * 
-	 * def run_from_save(output_name, num_runs):
-	#add in to do multiple runs
-	nodes = load_network(output_name)
-	for i in range(0,num_runs):
-		print(i)
-		ts = run_network(nodes)
-		while np.count_nonzero(ts[-1] == True) <= 300: # if less than x people tweeted, redo the analysis
-			ts = run_network(nodes)
-		start_idx = ts[0].tolist().index(True)
-		run_analysis(ts, nodes, output_name)
-	 * 
-	 */
 	
 	public void tweet(Node user, boolean[] hasTweeted, int[] numSeen, int[] lastSeen, int ts) {
 		hasTweeted[user.id] = true;
+		
 		for(int id : user.followers) {
 			numSeen[id]++;
 			lastSeen[id] = ts;
@@ -57,7 +43,7 @@ public class NetworkRunner {
 		
 		int step = 1;
 		int count = 0;
-		double baseProb = 1;
+		double baseProb = .02;
 		while(count <= 5) {
 			ArrayList<Node> toTweet = new ArrayList<Node>();
 			for(int i = 0; i < net.size; i++) {
@@ -68,20 +54,26 @@ public class NetworkRunner {
 				double time_past = step - lastSeen[i];
 				
 				double prob = baseProb/(time_past*2);
-				
-				
+
 				if (r < prob) {
-					System.out.println("tweet");
 					toTweet.add(net.getNodeById(i));
-				}
-				
+				} 
 			}
 			tweet(toTweet, hasTweeted, numSeen, lastSeen, step);
 			timeSteps.add(toTweet);
-			count++;
+			
+			if(toTweet.isEmpty()) 
+				count++;
+			else
+				count = 0;
+			step++;
 		}
 		
 		return timeSteps;
+	}
+
+	public void saveRun(ArrayList<ArrayList<Node>> timeSteps) {
+		
 	}
 	
 	public static void main(String[] args) throws IOException {
