f3e15f5066947cf03662acb546cb420259c96dea
adam <adamschunk@gmail.com>
2018-05-17 13:36:36 -0400

    updated runner

--- NetworkGenerator/src/network/CentralRunner.java
+++ NetworkGenerator/src/network/CentralRunner.java
@@ -4,24 +4,30 @@ import java.io.File;
 
 public class CentralRunner {
 	public static void main(String[] args) throws Exception {
-		String outputDir = "50000_cluster5_fill70/";
+		String networkDir = "50000_rndm/";
+		String outputDir = "50000_rndm/socialPressure3/";
 
 		if (!new File(outputDir).exists()) {
 			new File(outputDir).mkdirs();
 		}
 		
-		int size = 50000;
-		double rndmFillPercent = .7;
-		int clusteringWeight = 5;
-		NetworkGenerator ng = new NetworkGenerator();
-		Network net = ng.generateNetwork(rndmFillPercent, clusteringWeight, size, outputDir);
-
-		int numRuns = 1000;
-		NetworkRunner runner = new NetworkRunner(0, 300, outputDir);
+//		int size = 30000;
+//		double rndmFillPercent = .7;
+//		int clusteringWeight = 5;
+//		NetworkGenerator ng = new NetworkGenerator();
+//		Network net = ng.generateNetwork(rndmFillPercent, clusteringWeight, size, outputDir);
+		
+		
+		
+		//social pressure of 0 or 1 have no effect, 
+		//higher numbers mean lower base prob of tweet but more effect from seen.
+		int numRuns = 900;
+		double socialPressure = 3;
+		int minRetweets = 400;
+		NetworkRunner runner = new NetworkRunner(socialPressure, minRetweets, networkDir);
 		runner.runMultiple(numRuns, outputDir);
 
-		RunAnalyzer ra = new RunAnalyzer(outputDir);
-		ra.countOccurances();
-
+//		RunAnalyzer ra = new RunAnalyzer(outputDir);
+//		ra.findClusters();
 	}
 }
